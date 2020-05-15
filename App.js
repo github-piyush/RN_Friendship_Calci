6bb4f032a3fbc826e2fe4716971d5275bb8ce671
Piyush Deshmukh <piyushd687@gmail.com>
2020-05-15 22:00:08 +0530

    first commit

---
+++ App.js
@@ -0,0 +1,63 @@
+import React from 'react';
+import { StyleSheet, Text, View, ScrollView, Image } from 'react-native';
+import { TextInput, Appbar , Button} from 'react-native-paper'
+
+class App extends React.Component {
+
+  state = {
+    name1: '',
+    name2: ''
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+
+        <Appbar.Header style={{backgroundColor:'purple'}}>
+         
+          <Appbar.Content
+            title="Friendship % Calculator"
+            subtitle="masterapps.co"
+          />
+          
+        </Appbar.Header> 
+
+
+        <Text style={{ fontSize: 20, color: "yellow" }}>Please enter the details here...</Text>
+        <TextInput
+          label='Name 1'
+          value={this.state.hname}
+          onChangeText={text => this.setState({ text })}
+        />
+        <TextInput
+          label='Name 2'
+          value={this.state.sname}
+          onChangeText={text => this.setState({ text })}
+        />
+        <Button style={{backgroundColor:'yellow'}} icon="camera" mode="contained" onPress={() => console.log('Pressed')}>
+          <Text style={{color:'blue'}}>Calculate</Text>
+        </Button>
+      </View>
+    );
+  }
+
+}
+
+
+
+export default App;
+
+
+//if we dont write StyleSheet.create then also it will work, but it is helpful in error checking
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor:'#cc00ff',
+
+    flexDirection: "column",       //how we will justifyContent
+    // justifyContent: "center",      //centerize flexDirection-wise
+    // alignItems: "center"           //centerize the opposite of flexdirection
+  },
+});
+
+//for more, type FLEXBOX REACT-NATIVE in GOOGLE
